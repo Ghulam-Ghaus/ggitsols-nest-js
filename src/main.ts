ab31499da1593ae93c1a575cfee3ccbd60379348
khanaftabrana1-hub <khanaftabrana1@gmail.com>
2026-08-18 06:15:26 -0700

    i created admin update

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ async function bootstrap() {
 
   const port = Number(process.env.PORT);
   const app = await NestFactory.create(AppModule);
+   // Allow CORS from all origins
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(port);
   console.log('app is running on port :', port);
 }
